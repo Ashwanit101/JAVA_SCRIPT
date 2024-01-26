c17ce9739585bb296034d338f27c3b9f1f92ecb8
Ashwanit101 <148317389+Ashwanit101@users.noreply.github.com>
2024-01-26 09:05:22 +0000

    data types

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,28 @@
+  "use scrict"; //treat all JS code as newer version
+//   alert(3+3) we are using node js not browser
+ console.log(3+
+    
+    3)
+    // code readability should be high
+    
+    let name="Ashwani"
+    let age=18
+    let isloggined=true
+    let state
+  
+    // Data types
+          // Primitive data types
+    // number => range 2 to power 53
+    // Bigint =>used not generally but for large numbers
+    // boolean=>true/false
+    // null=>standalone value=>empty 
+    // undefined=>value not given
+    // symbol=>unique
+
+
+    // object
+    console.log(typeof "Ashwani")
+    console.log(typeof age)
+    console.log(typeof null)//type object
+    console.log(typeof undefined)//type undefined
+    console.log(typeof symbol )//type undefined
