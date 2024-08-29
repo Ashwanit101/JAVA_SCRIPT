31a7f887c8d384924453899083c3139845361022
Ashwanit101 <148317389+Ashwanit101@users.noreply.github.com>
2024-08-29 18:22:55 +0000

    operations

--- 01basics/03conversionOperations.js
+++ 01basics/03conversionOperations.js
@@ -26,3 +26,41 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+//  **********OPERATIONS********//
+
+let value=2
+let negvalue=-value
+console.log(negvalue);
+// arithmetic operations
+console.log(2+2);
+console.table([2+2,2*2,2-2,2/2,2*3,2**2,2/3,2%3]);
+let str1="Hello"
+let str2=" Ashwani"
+let str3=str1 +str2 
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2"+2);
+// if first operand is string then whole calculation takes place converting all as string
+console.log(1+2+"2");
+// if numbers occur before string then first  calculation takes place then both concatenate as string
+console.log("1"+2+2);
+console.log((3+4)*6%3); // bad practice
+console.log(+true);
+console.log(+"")
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1)
+let gamecounter=100
+gamecounter++;
+// post precedence
+console.log(gamecounter)
+
+
+
+
+
+
+
