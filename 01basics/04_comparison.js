7814f529a9cc7eb3ced38dac105d4bd30756104b
Ashwanit101 <148317389+Ashwanit101@users.noreply.github.com>
2024-08-29 18:47:52 +0000

    comparsions

---
+++ 01basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+ console.log("2" > 1);
+ console.log("02" > 1);
+ // it converted 2 and 02 into number
+
+ console.log(null>0)
+ console.log(null==0)
+ console.log(null>=0)
+ console.log(null<0)
+ // comparsion and equality operator work differently 
+ // comparsion convert null to 0 while equality do not convert
+ console.log(undefined>0)
+ console.log(undefined==0)
+ console.log(undefined>=0)
+ console.log(undefined<0)
+
+
+ //strict check ===
+   // it checks both data type and number
+ console.log("2"===2);
+
+
+
+
