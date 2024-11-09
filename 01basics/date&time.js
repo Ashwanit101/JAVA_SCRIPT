82ac26e000ea09f61ea56954195fd9208273168e
Ashwanit101 <148317389+Ashwanit101@users.noreply.github.com>
2024-11-09 12:47:47 +0000

    date and time new

--- 01basics/date&time.js
+++ 01basics/date&time.js
@@ -1,24 +1,24 @@
  //Dates
  let myDate=new Date();
-//  console.log(myDate);
-//  console.log(myDate.toString());
-//  console.log(myDate.toDateString());
-//  console.log(myDate.toLocaleString());
-//  console.log(typeof(myDate));
-// let mycreatedDate=new Date(2023,0,23);// here counting of month start from 0 indexing
+ console.log(myDate);
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toLocaleString());
+ console.log(typeof(myDate));
+let mycreatedDate=new Date(2023,0,23);// here counting of month start from 0 indexing
 
-// let mycreatedDate=new Date(2024,8,2,5,6);// here 5 and 6 will be considered for hour and min
+let mycreatedDate=new Date(2024,8,2,5,6);// here 5 and 6 will be considered for hour and min
 let mycreatedDate=new Date("01-12-2024");
-//  console.log(mycreatedDate.toLocaleString());
+ console.log(mycreatedDate.toLocaleString());
 
 // !!!!!IMPORTANT!!!!
 // in quizes where comparison is required of time (in milliseconds)
  let myTimeStamp=Date.now();
 
-//  console.log(myTimeStamp);
-//  console.log(mycreatedDate.getTime())
+ console.log(myTimeStamp);
+ console.log(mycreatedDate.getTime())
  // how to convert millisec to sec
-//  console.log(Math.floor(Date.now()/1000));
+ console.log(Math.floor(Date.now()/1000));
  
  let newDate=new Date();
  console.log(newDate);
